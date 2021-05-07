957acb76f8a6cf6d9c6ed0426250f53bf6599815
rafaelmarrasilva <rafaelmarrasilva@gmail.com>
2021-05-07 18:09:07 -0300

    Create Resultado.cs
    primeiro commit

---
+++ Analisador/Entities/Resultado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analisador.Entities
+{
+    class Resultado
+    {
+        public int Concurso { get; set; }
+        public DateTime Data { get; set; }
+        public List<int> Numeros { get; set; }
+
+        public Resultado(int concurso, DateTime data, List<int> numeros)
+        {
+            Concurso = concurso;
+            Data = data;
+            Numeros = numeros;
+        }
+
+        public Resultado()
+        {
+
+        }
+    }
+}
