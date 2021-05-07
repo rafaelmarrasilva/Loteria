8c6f8d491e2080ec783f766bdb9a419094966f41
rafaelmarrasilva <rafaelmarrasilva@gmail.com>
2021-05-07 18:09:13 -0300

    Create Combinacao.cs
    primeiro commit

---
+++ Analisador/Entities/Combinacao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analisador.Entities
+{
+    class Combinacao
+    {
+        public string IdCombinacao { get; set; }
+        public List<int> SeqCombinada { get; set; }
+        public int QtdRepeticoes { get; private set; }
+
+        public Combinacao()
+        {
+
+        }
+
+        public Combinacao(string idCombinacao, List<int> seqCombinada)
+        {
+            IdCombinacao = idCombinacao;
+            SeqCombinada = seqCombinada;
+        }
+
+        public void addRepeticao(int qtdRepeticoes)
+        {
+            QtdRepeticoes = QtdRepeticoes + qtdRepeticoes;
+        }
+    }
+}
