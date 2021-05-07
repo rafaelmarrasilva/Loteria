2b80e06d47313ad3fa2d3d5025db36cf53c4e68e
rafaelmarrasilva <rafaelmarrasilva@gmail.com>
2021-05-07 18:08:36 -0300

    Create Similaridade.cs
    primeiro commit

---
+++ GeradorNumeros/Entities/Similaridade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loteria.Entities
+{
+    class Similaridade
+    {
+        public static void Similar(List<JogoGerado> pLista, int qtdSimilar)
+        {
+            //int tamLista = pLista.Count();
+            int i = 0;
+            int j = 0;
+            foreach (var p in pLista)
+            {
+                var listaCom = new HashSet<int>(p.ListaNumeros);
+
+                foreach (var s in pLista)
+                {
+                    if (i != j)
+                    {
+                        var listaSec = new HashSet<int>(s.ListaNumeros);
+
+                        listaSec.IntersectWith(listaCom);
+
+                        Console.WriteLine("Sequencial " + s.Sequencial + " é similar (" + listaSec.Count + ") ao Sequencial " + p.Sequencial);
+
+                        j++;
+                        listaSec.Clear();
+                    }
+                }
+                j = 0;
+                i++;
+                listaCom.Clear();
+            }
+        }
+    }
+}
