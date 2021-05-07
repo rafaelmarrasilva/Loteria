7772b7f2b6078af18b6be895402125a53e5b9bd4
rafaelmarrasilva <rafaelmarrasilva@gmail.com>
2021-05-07 18:08:49 -0300

    Create JogoGerado.cs
    primeiro commit

---
+++ GeradorNumeros/Entities/JogoGerado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loteria.Entities
+{
+    class JogoGerado
+    {
+        public int Sequencial { get; set; }
+        public DateTime Data { get; set; }
+        public List<int> ListaNumeros { get; set; } = new List<int>(Gerador.GerarJogo());
+
+        public JogoGerado(int sequencial, DateTime data)
+        {
+            Sequencial = sequencial;
+            Data = data;
+        }
+    }
+}
