f13c712297921a235b68b788a81c14f8a110760d
rafaelmarrasilva <rafaelmarrasilva@gmail.com>
2021-05-10 18:56:56 -0300

    Create Jogo.cs
    renomeando a classe para um nome mas apropriado

---
+++ GeradorNumeros/Entities/Jogo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loteria.Entities
+{
+    public class Jogo
+    {
+        public int Sequencial { get; set; }
+        public DateTime Data { get; set; }
+        public List<int> ListaNumeros { get; set; } = new List<int>();
+
+        public Jogo()
+        {
+
+        }
+
+        public Jogo(int sequencial, DateTime data, List<int> listaNumeros)
+        {
+            Sequencial = sequencial;
+            Data = data;
+            ListaNumeros = listaNumeros;
+        }
+
+        public void addNumero(int numero)
+        {
+            ListaNumeros.Add(numero);
+        }
+    }
+}
